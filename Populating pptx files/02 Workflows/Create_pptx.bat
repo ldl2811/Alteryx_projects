ca29aa62df8727e3165dd6ac560a87d5b1d980ca
lb930 <59416844+lb930@users.noreply.github.com>
2020-10-04 11:14:46 +0200

    Added yxzp file and fixed date format

--- Populating pptx files/02 Workflows/Create_pptx.bat
+++ Populating pptx files/02 Workflows/Create_pptx.bat
@@ -1,15 +1,15 @@
-2020-09-30
+2020-10-03
 set PATH=%PATH%;C:\Program Files\7-Zip
 
-cd C:\Users\bezlui\Documents\Projects\Populating pptx files\02 Workflows\02_Replacement\ppt\embeddings
+cd C:\Users\bezlui\Documents\GitHub\Alteryx-projects\Populating pptx files\02 Workflows\02_Replacement\ppt\embeddings
 del *.bak
 
-cd C:\Users\bezlui\Documents\Projects\Populating pptx files\02 Workflows\02_Replacement
+cd C:\Users\bezlui\Documents\GitHub\Alteryx-projects\Populating pptx files\02 Workflows\02_Replacement
 7z a ..\Company_presentation.zip -tzip -r *.*
 
 cd..
-ren Company_presentation.zip Company_presentation_2020-09-30.pptx
+ren Company_presentation.zip Company_presentation_2020-10-03.pptx
 
-move /Y Company_presentation_2020-09-30.pptx ..\"03 PPTX"
+move /Y Company_presentation_2020-10-03.pptx ..\"03 PPTX"
 
 
